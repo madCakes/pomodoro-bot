dccb2353f4bb616d41eed7cace5d6b674ac1c8aa
madCakes <jason.pbrooks@hotmail.com>
2023-01-26 18:51:17 +0000

    add test pong commands

---
+++ commands/pom.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("pom")
+    .setDescription(
+      "Provides time spent voice (TODO: time in minutes/hours, time in pomodoro segments (30 min))"
+    ),
+  async execute(interaction) {
+    // interaction.user is the object representing the User who ran the command
+    // interaction.member is the GuildMember object, which represents the user in the specific guild
+    await interaction.reply(
+      `This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`
+    );
+  },
+};
