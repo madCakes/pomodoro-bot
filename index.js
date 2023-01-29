bf94230c15a16698d4bf8758dd8424525ca4ab35
madCakes <jason.pbrooks@hotmail.com>
2023-01-29 19:30:54 +0000

    refactor events

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Require the necessary discord.js classes
 const fs = require("node:fs");
 const path = require("node:path");
-const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
 const { token } = require("./config.json");
 
 // Create a new client instance
@@ -10,7 +10,7 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 // Add commands property as a Map
 client.commands = new Collection();
 
-// Read filenames
+// Read command files
 const commandsPath = path.join(__dirname, "commands");
 const commandFiles = fs
   .readdirSync(commandsPath)
@@ -30,38 +30,24 @@ for (const file of commandFiles) {
   }
 }
 
-// testing bot online
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!.`);
-});
+// Read event files
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
 
-// Create listener for command interactions
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-
-  const command = interaction.client.commands.get(interaction.commandName);
-
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
   }
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({
-      content: "There was an error while executing this command!",
-      ephemeral: true,
-    });
-  }
-});
-
+}
 // Log in to Discord
 client.login(token);
 
-// PROG
-// Command Handling https://discordjs.guide/creating-your-bot/command-handling.html#loading-command-files
 // TODO
 // Track time on voice channel join (voice state change)
 // Track time on voice channel leave OR !pom command (voice state change)
